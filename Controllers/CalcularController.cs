eca39f8f962b0ba21fb59424468f0087938faf93
daniel20max <51431192+daniel20max@users.noreply.github.com>
2021-08-06 18:02:20 -0300

    Initial commit

---
+++ Controllers/CalcularController.cs
@@ -0,0 +1,39 @@
+﻿using DevSoftPlayerApi2.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DevSoftPlayerApi2.Controllers
+{
+    [ApiController]
+    [Route("Index")]
+    public class CalcularController : Controller
+    {
+        ApiExterna _api;
+        public CalcularController(ApiExterna api)
+        {
+            _api = api;
+        }
+        [HttpPost]
+        [Route("JurosApiExterna")]
+        public IActionResult CalcularJurosExterno(double valorInicial, int tempoDecorrido)
+        {
+            double juros = _api.ReturnGet();
+            double valorJuros = Math.Pow((1 + juros), tempoDecorrido);
+            double valorFinal = valorInicial * valorJuros;
+
+            return Ok(valorFinal);
+        }
+        [HttpGet]
+        [Route("Git")]
+        public IActionResult ShowMeTheCode()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/ApiExterna.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DevSoftPlayerApi2.Services
+{
+    public class ApiExterna
+    {
+        public double ReturnGet()
+        {
+            var requestWeb = WebRequest.CreateHttp("https://localhost:44344/Index/TaxaJuros");
+            requestWeb.Method = "GET";
+            requestWeb.UserAgent = "RequestWeb";
+            var returnGet = requestWeb.GetResponse();
+            var streamDados = returnGet.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamDados);
+            object objectResponse = streamReader.ReadToEnd();
+            double post = JsonConvert.DeserializeObject<double>(objectResponse.ToString());
+            return post;
+        }
+    }
+}
